67f748b9106d2d605dad446f0380290be375fd15
Mayank <innovativecreations195@gmail.com>
2024-06-21 21:04:30 +0530

    getting the fact, now moving towards the meme

--- sillyCats/script.js
+++ sillyCats/script.js
@@ -3,6 +3,8 @@ async function getRandomCatFact() {
     const factResponse = await fetch('https://catfact.ninja/fact');
     const factData = await factResponse.json();
     console.log(factData);
+    document.getElementById('cat-fact').innerText = factData.fact;
+    
 }
 
 
