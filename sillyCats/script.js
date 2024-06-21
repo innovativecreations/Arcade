63a1a74b99b69b0d7e3fa7ef811630c8c2f568e0
Mayank <innovativecreations195@gmail.com>
2024-06-21 21:50:47 +0530

    got the img

--- sillyCats/script.js
+++ sillyCats/script.js
@@ -4,7 +4,13 @@ async function getRandomCatFact() {
     const factData = await factResponse.json();
     console.log(factData);
     document.getElementById('cat-fact').innerText = factData.fact;
-    
+
+   const memeResponse = await fetch('https://api.thecatapi.com/v1/images/search');
+        if (!memeResponse.ok) {
+            throw new Error('Failed to fetch cat meme');
+        }
+        const memeData = await memeResponse.json();
+        document.getElementById('cat-meme').src = memeData[0].url;
 }
 
 
