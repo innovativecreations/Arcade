d9e012b07f550ec2ef5aa9b98cb5ccb6c90e5c9c
Mayank <innovativecreations195@gmail.com>
2024-06-22 06:32:25 +0530

    change theme to black

--- sillyCats/script.js
+++ sillyCats/script.js
@@ -2,16 +2,14 @@ async function getRandomCatFact() {
 
     const factResponse = await fetch('https://catfact.ninja/fact');
     const factData = await factResponse.json();
-    console.log(factData);
-    document.getElementById('cat-fact').innerText = factData.fact;
+    const catFactElement = document.getElementById('cat-fact');
+    
+    catFactElement.innerText = factData.fact;
 
-   const memeResponse = await fetch('https://api.thecatapi.com/v1/images/search');
-        if (!memeResponse.ok) {
-            throw new Error('Failed to fetch cat meme');
-        }
-        const memeData = await memeResponse.json();
-        document.getElementById('cat-meme').src = memeData[0].url;
+
+    const memeResponse = await fetch('https://api.thecatapi.com/v1/images/search');
+    const memeData = await memeResponse.json();
+    document.getElementById('cat-meme').src = memeData[0].url;
 }
-
 
 getRandomCatFact();
