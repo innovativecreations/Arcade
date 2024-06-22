1a35c3985109a2e3ded561b682b97f9d9c04f3fa
Mayank <innovativecreations195@gmail.com>
2024-06-22 21:49:52 +0530

    wring is done and now testing the parts

---
+++ voice controlled car/testing parts/motors/motors.ino
@@ -0,0 +1,17 @@
+int motor1pin1 = 4;
+int motor1pin2 = 5;
+
+int motor2pin1 = 6;
+int  motor2pin2 = 7;
+
+void setup() {
+
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+
+
+}
+
+void loop() {
+
+}
