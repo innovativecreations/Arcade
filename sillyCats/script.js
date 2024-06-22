d081518a3d10d3db148f50d274dce0aa29f9828a
Mayank <innovativecreations195@gmail.com>
2024-06-22 07:29:14 +0530

    add loader

--- sillyCats/script.js
+++ sillyCats/script.js
@@ -1,24 +1,31 @@
 async function getRandomCatFact() {
+    const loader = document.getElementById('loader');
+    const catFact = document.getElementById('cat-fact');
+    const catMeme = document.getElementById('cat-meme');
+
+    loader.style.display = 'block';
+    catFact.style.opacity = 0;
+    catMeme.style.opacity = 0;
 
     const factResponse = await fetch('https://catfact.ninja/fact');
     const factData = await factResponse.json();
-    const catFactElement = document.getElementById('cat-fact');
-    
-    catFactElement.innerText = factData.fact;
+    catFact.innerText = factData.fact;
 
-    
     const factLength = factData.fact.length;
-    const minFontSize = 1; 
-    const maxFontSize = 2.5; 
-    const maxFactLength = 200; 
+    const minFontSize = 1;
+    const maxFontSize = 2.5;
+    const maxFactLength = 200;
 
     const fontSize = Math.max(minFontSize, maxFontSize - (factLength / maxFactLength) * (maxFontSize - minFontSize));
-    catFactElement.style.fontSize = `${fontSize}em`;
-
+    catFact.style.fontSize = `${fontSize}em`;
 
     const memeResponse = await fetch('https://api.thecatapi.com/v1/images/search');
     const memeData = await memeResponse.json();
-    document.getElementById('cat-meme').src = memeData[0].url;
+    catMeme.src = memeData[0].url;
+
+    loader.style.display = 'none';
+    catFact.style.opacity = 1;
+    catMeme.style.opacity = 1;
 }
 
 getRandomCatFact();
