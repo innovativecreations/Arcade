821e057b50b859356575ee97c1d0df3d0fbcd309
Mayank <innovativecreations195@gmail.com>
2024-06-22 07:02:26 +0530

    make the fact dynamic and start adding a loader

--- sillyCats/script.js
+++ sillyCats/script.js
@@ -6,6 +6,15 @@ async function getRandomCatFact() {
     
     catFactElement.innerText = factData.fact;
 
+    
+    const factLength = factData.fact.length;
+    const minFontSize = 1; 
+    const maxFontSize = 2.5; 
+    const maxFactLength = 200; 
+
+    const fontSize = Math.max(minFontSize, maxFontSize - (factLength / maxFactLength) * (maxFontSize - minFontSize));
+    catFactElement.style.fontSize = `${fontSize}em`;
+
 
     const memeResponse = await fetch('https://api.thecatapi.com/v1/images/search');
     const memeData = await memeResponse.json();
