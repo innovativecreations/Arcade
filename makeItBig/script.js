754c0d2fabe9bd02ce6d0b67638c00d02f2bc0f5
Mayank <innovativecreations195@gmail.com>
2024-06-23 12:08:51 +0530

    add default img and some styling

--- makeItBig/script.js
+++ makeItBig/script.js
@@ -1,7 +1,8 @@
 document.getElementById('submit-button').addEventListener('click', function() {
-    const imageUrl = document.getElementById('image-url').value;
-    if (imageUrl) {
-        // Open new page with the image
-        window.open('resize.html?url=' + encodeURIComponent(imageUrl), '_blank');
+    let imageUrl = document.getElementById('image-url').value;
+    if (!imageUrl) {
+        imageUrl = 'potato/1.png';
     }
+
+    window.open('resize.html?url=' + encodeURIComponent(imageUrl), '_blank');
 });
