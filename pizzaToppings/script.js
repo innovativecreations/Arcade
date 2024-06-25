fdc0c82c2963af64069138f805fb4917ae3dd9de
Mayank <innovativecreations195@gmail.com>
2024-06-25 23:11:47 +0530

    starting pizzaToppings

---
+++ pizzaToppings/script.js
@@ -0,0 +1,16 @@
+document.getElementById('generate-button').addEventListener('click', function() {
+    const toppings = [
+        "Marshmallow and Pickles",
+        "Chocolate Sprinkles and Bacon",
+        "Candy Corn and Jalapenos",
+        "Gummy Bears and Anchovies",
+        "Peanut Butter and Olives",
+        "Sardines and Whipped Cream",
+        "Banana Slices and Hot Sauce",
+        "Fried Insects and Cheese",
+        "Cotton Candy and Pepperoni",
+        "Popcorn and Mushrooms",
+    ];
+
+
+});
