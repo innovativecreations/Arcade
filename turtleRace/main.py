010a4addbd98b9fe1e8931fb2bcc0fbf5875c95d
Mayank <innovativecreations195@gmail.com>
2024-06-25 15:31:25 +0530

    make the turtle run

--- turtleRace/main.py
+++ turtleRace/main.py
@@ -1,34 +1,31 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+colours = ["red", "orange", "yellow", "green", "blue", "purple"]
+ordinates = [-100, -60, -20, 20, 60, 100]
 screen = Screen()
+screen.setup(width=500, height=400)
+prediction = screen.textinput(title="Make your bet", prompt="Which turtle will win the game? Type the colour")
+sara_kachua = []
 
 
-def move_forwards():
-    tim.forward(10)
+for i in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colours[i])
+    tim.penup()
+    tim.goto(x=-230, y=ordinates[i])
+    sara_kachua.append(tim)
+
+if prediction in colours:
+    not_game_over = True
+else:
+    print("You had entered wrong command. Please try again")
+while not_game_over:
+    for i in range(0, 6):
+        kachua = sara_kachua[i]
+        distance = random.randint(0, 10)
+        kachua.forward(distance)
 
 
-def move_backward():
-    tim.backward(10)
 
-
-def change_left_angle():
-    tim.left(2)
-
-
-def change_right_angle():
-    tim.right(2)
-
-
-def reset():
-    tim.goto(0,0)
-    tim.clear()
-
-
-screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backward)
-screen.onkey(key="a", fun=change_left_angle)
-screen.onkey(key="d", fun=change_right_angle)
-screen.onkey(key="c", fun=reset)
 screen.exitonclick()
