6347695067ff672fd5ca9f28e37e55559604225f
Mayank <innovativecreations195@gmail.com>
2024-06-25 12:35:50 +0530

    draw dashed line

--- crazyDesigns/main.py
+++ crazyDesigns/main.py
@@ -1,15 +1,27 @@
+# import turtle as t
+
+# timmy_the_turtle = t.Turtle()
+# # timmy_the_turtle.shape("turtle")
+# # timmy_the_turtle.color("red")
+# # timmy_the_turtle.forward(100)
+# # timmy_the_turtle.backward(200)
+# # timmy_the_turtle.right(90)
+# # timmy_the_turtle.left(180)
+# # timmy_the_turtle.setheading(0)
+
+
+# for i in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+
+########### dashed line ###########
 import turtle as t
 
-timmy_the_turtle = t.Turtle()
-# timmy_the_turtle.shape("turtle")
-# timmy_the_turtle.color("red")
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.backward(200)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.left(180)
-# timmy_the_turtle.setheading(0)
+tim = t.Turtle()
 
 
-for i in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+for i in range(20):
+    tim.penup()
+    tim.forward(5)
+    tim.pendown()
+    tim.forward(5)
