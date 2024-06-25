e7d0e8c3a22059bde515f19e6b1e37e76c9e28f9
Mayank <innovativecreations195@gmail.com>
2024-06-25 14:01:58 +0530

    draw circle with circles

--- crazyDesigns/main.py
+++ crazyDesigns/main.py
@@ -2,20 +2,24 @@ import turtle as t
 import random
 
 tim = t.Turtle()
-
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-angles = [0, 90, 180, 270]
 t.colormode(255)
-tim.shape("turtle")
-def color():
+
+
+def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    rgb = (r, g, b)
-    tim.color(rgb)
+    color = (r, g, b)
+    return color
+########### Challenge 5 - Spirograph ########
 # tim.speed("fastest")
-tim.pensize(10)
-for i in range(270):
-    tim.forward(30)
-    tim.right(random.choice(angles))
-    color()
+degree = 7
+while True:
+# for i in range(int(360/degree)):
+    tim.circle(100)
+    tim.left(degree)
+    color = random_color()
+    tim.color(color)
+
+screen = t.Screen()
+screen.exitonclick()
