10cdaf5f298c113b79769195b0e26a86753b7a12
Mayank <innovativecreations195@gmail.com>
2024-06-25 17:59:29 +0530

    game completed

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -1,27 +1,60 @@
 from turtle import Screen
-from snake import Snake
-import time
+import time as t
+from saap import Snake
+from khana import Khana
+from points import Ankh
+
+saap_ka_size = 3
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My Snake Game")
+screen.title("Saap_ka_khel")
 screen.tracer(0)
 
-snake = Snake()
+khana = Khana()
+saap = Snake()
+ankh = Ankh()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey(saap.up, "Up")
+screen.onkey(saap.down, "Down")
+screen.onkey(saap.right, "Right")
+screen.onkey(saap.left, "Left")
 
-game_is_on = True
-while game_is_on:
+# saap_ka_sariir = saap.saap_ka_nirman
+
+screen.update()
+
+khel_chalu = True
+while khel_chalu:
     screen.update()
-    time.sleep(0.1)
+    t.sleep(0.1)
 
-    snake.move()
+    saap.saap_hiil()
+
+    #khana thus raha hai
+    if saap.saap_ka_muh.distance(khana) < 15:
+        khana.pakwan()
+        ankh.ginoh()
+        saap.motapa()
+
+    #diwal se thuk gaya
+    if saap.saap_ka_muh.xcor() > 280 or saap.saap_ka_muh.xcor() < -280 or saap.saap_ka_muh.ycor() > 280 or \
+            saap.saap_ka_muh.ycor() < -280:
+        ankh.aukaat_bada_kya()
+        saap.waapsi()
+
+    #paer par kulhadi
+    for i in saap.saap_ka_sariirr[1:]:
+        if saap.saap_ka_muh.distance(i) < 1:
+            ankh.aukaat_bada_kya()
+            saap.waapsi()
+
+
+
+
+ankh.ghar_jaao()
 
 
 screen.exitonclick()
