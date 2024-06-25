001bc538a8b04c7bfa4ad3a3fb4fb275a9e6af2b
Mayank <innovativecreations195@gmail.com>
2024-06-25 11:20:42 +0530

    getting an error

--- coffeeMachine/main.py
+++ coffeeMachine/main.py
@@ -1,7 +1,10 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
 menu = Menu()
 menu_item = MenuItem()
 coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
+coffee_maker.report()
