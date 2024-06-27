9f566312e3eaa9d328e76b4fc477e0ae5f77de64
Mayank <innovativecreations195@gmail.com>
2024-06-27 16:16:48 +0530

    add music

--- pet/script.js
+++ pet/script.js
@@ -1,5 +1,45 @@
+// document.getElementById('dance-button').addEventListener('click', function() {
+//     const rock = document.getElementById('pet-rock');
+//     const music = document.getElementById('dance-music');
+//     let danceInterval;
+
+//     const danceMoves = [
+//         { transform: 'translateX(50px)' },
+//         { transform: 'translateX(-50px)' },
+//         { transform: 'translateY(50px)' },
+//         { transform: 'translateY(-50px)' },
+//         { transform: 'rotate(20deg)' },
+//         { transform: 'rotate(-20deg)' }
+//     ];
+
+//     const startDancing = () => {
+//         music.play();
+//         danceInterval = setInterval(() => {
+//             const randomMove = danceMoves[Math.floor(Math.random() * danceMoves.length)];
+//             rock.style.transform = randomMove.transform;
+//         }, 500);
+//     };
+
+//     const stopDancing = () => {
+//         clearInterval(danceInterval);
+//         rock.style.transform = '';
+//         music.pause();
+//         music.currentTime = 0;
+//     };
+
+//     startDancing();
+//     setTimeout(stopDancing, 5000);
+// });
+
+
+
+
+let isDancing = false;
+let danceInterval;
+
 document.getElementById('dance-button').addEventListener('click', function() {
     const rock = document.getElementById('pet-rock');
+    const music = document.getElementById('dance-music');
 
     const danceMoves = [
         { transform: 'translateX(50px)' },
@@ -10,10 +50,18 @@ document.getElementById('dance-button').addEventListener('click', function() {
         { transform: 'rotate(-20deg)' }
     ];
 
-    const randomMove = danceMoves[Math.floor(Math.random() * danceMoves.length)];
-    rock.style.transform = randomMove.transform;
-
-    setTimeout(() => {
+    if (!isDancing) {
+        music.play();
+        danceInterval = setInterval(() => {
+            const randomMove = danceMoves[Math.floor(Math.random() * danceMoves.length)];
+            rock.style.transform = randomMove.transform;
+        }, 500);
+        isDancing = true;
+    } else {
+        clearInterval(danceInterval);
         rock.style.transform = '';
-    }, 500);
+        music.pause();
+        music.currentTime = 0;
+        isDancing = false;
+    }
 });
