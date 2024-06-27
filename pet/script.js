11dfef7ce2cb636a454ce21d409517bf294bc163
Mayank <innovativecreations195@gmail.com>
2024-06-27 17:19:19 +0530

    some changes

--- pet/script.js
+++ pet/script.js
@@ -1,41 +1,13 @@
-// document.getElementById('dance-button').addEventListener('click', function() {
-//     const rock = document.getElementById('pet-rock');
-//     const music = document.getElementById('dance-music');
-//     let danceInterval;
-
-//     const danceMoves = [
-//         { transform: 'translateX(50px)' },
-//         { transform: 'translateX(-50px)' },
-//         { transform: 'translateY(50px)' },
-//         { transform: 'translateY(-50px)' },
-//         { transform: 'rotate(20deg)' },
-//         { transform: 'rotate(-20deg)' }
-//     ];
-
-//     const startDancing = () => {
-//         music.play();
-//         danceInterval = setInterval(() => {
-//             const randomMove = danceMoves[Math.floor(Math.random() * danceMoves.length)];
-//             rock.style.transform = randomMove.transform;
-//         }, 500);
-//     };
-
-//     const stopDancing = () => {
-//         clearInterval(danceInterval);
-//         rock.style.transform = '';
-//         music.pause();
-//         music.currentTime = 0;
-//     };
-
-//     startDancing();
-//     setTimeout(stopDancing, 5000);
-// });
-
-
-
-
 let isDancing = false;
 let danceInterval;
+let imageInterval;
+
+const rockImages = [
+    'dino.png',
+    'dino1.png',
+    'dino2.png',
+    'dino3.png'
+];
 
 document.getElementById('dance-button').addEventListener('click', function() {
     const rock = document.getElementById('pet-rock');
@@ -56,10 +28,18 @@ document.getElementById('dance-button').addEventListener('click', function() {
             const randomMove = danceMoves[Math.floor(Math.random() * danceMoves.length)];
             rock.style.transform = randomMove.transform;
         }, 500);
+
+        imageInterval = setInterval(() => {
+            const randomImage = rockImages[Math.floor(Math.random() * rockImages.length)];
+            rock.style.backgroundImage = `url(${randomImage})`;
+        }, 1000);
+
         isDancing = true;
     } else {
         clearInterval(danceInterval);
+        clearInterval(imageInterval);
         rock.style.transform = '';
+        rock.style.backgroundImage = `url('dino.png')`;
         music.pause();
         music.currentTime = 0;
         isDancing = false;
